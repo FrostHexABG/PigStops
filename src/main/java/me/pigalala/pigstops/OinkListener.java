dc0d296c1184a6f0c519e032f160b2ce2867dbc2
JustBru00 <justbru00@gmail.com>
2024-09-02 18:05:27 -0400

    Closes #2

--- src/main/java/me/pigalala/pigstops/OinkListener.java
+++ src/main/java/me/pigalala/pigstops/OinkListener.java
@@ -2,6 +2,7 @@ package me.pigalala.pigstops;
 
 import com.destroystokyo.paper.event.player.PlayerStopSpectatingEntityEvent;
 import me.makkuusen.timing.system.api.TimingSystemAPI;
+import me.makkuusen.timing.system.heat.Lap;
 import me.pigalala.pigstops.pit.management.pitmodes.Pit;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -56,8 +57,20 @@ public class OinkListener implements Listener {
         	PigStops.getPlugin().getLogger().log(Level.SEVERE, "The default pit game has not been set. PigStops will not work correctly until a default pit game is set.");
         	e.getPlayer().sendMessage(Component.text().append(Component.text("The PigStop did not work as the default pit game is not set. Please ask an administrator to set the default pit game.", NamedTextColor.RED)));
         } else {
-        	 if(driver.get().getHeat().isActive() && driver.get().getCurrentLap() != null && !driver.get().getCurrentLap().isPitted()) pp.newPit(Pit.Type.REAL);
-        }      
+            // Issue #2 - Catch IndexOutofBoundException that appears when a player hasn't started a lap at all yet.
+            Lap lap = null;
+
+             try {
+                 lap = driver.get().getCurrentLap();
+             } catch (IndexOutOfBoundsException ex) {
+                 PigStops.getPlugin().getLogger().log(Level.WARNING, "Getting driver " + driver.get().getTPlayer().getNameDisplay() + "'s current lap caused an IndexOutOfBoundsException. Have they started a lap yet?");
+                 return;
+             }
+
+             if (driver.get().getHeat().isActive() && lap != null && !lap.isPitted()) {
+                 pp.newPit(Pit.Type.REAL);
+             }
+        }
     }
 
     @EventHandler
